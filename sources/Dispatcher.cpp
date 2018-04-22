dec54e8054a2eea6334e28fbd1fe867033aba00f
Reinual <michel.launier@icloud.com>
2018-04-22 19:47:52 -0400

    Add critical section to allow action activation from asynchronous sources such as IRQs.

--- sources/Dispatcher.cpp
+++ sources/Dispatcher.cpp
@@ -1,5 +1,6 @@
 #include "Dispatcher.h"
 #include "IAction.h"
+#include "ITarget.h"
 
 namespace LogikEdge { namespace React {
 
@@ -7,57 +8,85 @@ namespace LogikEdge { namespace React {
 
     int dummyCompare(Cdll&, Cdll&) { return 0; }
 
+    // ----------------------------------------------------------------------
+    /// @param toActivate   The action to be activated.
     void Dispatcher::activate(IOnEventAction& toActivate) {
+        ITarget::startCriticalSection();
         myOnEventQ.push(toActivate);
+        ITarget::endCriticalSection();
     }
+    // ----------------------------------------------------------------------
+    /// @param toActivate   The action to be activated.
     void Dispatcher::activate(IDelayedAction& toActivate) {
         toActivate.startTimer();
+        ITarget::startCriticalSection();
         myDelayQ.push(toActivate);
+        ITarget::endCriticalSection();
     }
+    // ----------------------------------------------------------------------
+    /// @param toActivate   The action to be activated.
     void Dispatcher::activate(IPeriodicAction& toActivate) {
         toActivate.startTimer();
+        ITarget::startCriticalSection();
         myPeriodicQ.push(toActivate);
+        ITarget::endCriticalSection();
     }
+    // ----------------------------------------------------------------------
+    /// @param toActivate   The action to be activated.
     void Dispatcher::activate(IBackgroundAction& toActivate) {
+        ITarget::startCriticalSection();
         myBackgroundQ.push(toActivate);
+        ITarget::endCriticalSection();
     }
 
+    // ----------------------------------------------------------------------
+    /// Runs the next highest priority action that is ready to run.
     void Dispatcher::runOnce() {
-        // -- First empty the OnEvent queue. --
+        // -- 1st priority: OnEvent actions. --
+        ITarget::startCriticalSection();
         Cdll* cdllToRun = myOnEventQ.pop();
         if(cdllToRun != 0) {
+            ITarget::endCriticalSection();
             IOnEventAction* toRun = static_cast<IOnEventAction*>(cdllToRun);
             toRun->run();
             return;
         }
 
+        // -- 2nd priority: delayed actions. --
         cdllToRun = myDelayQ.peek();
         if(cdllToRun != 0) {
             IDelayedAction* toRun = static_cast<IDelayedAction*>(cdllToRun);
             if(toRun->isReadyToRun()) {
                 myDelayQ.pop();
+                ITarget::endCriticalSection();
                 toRun->run();
+                return;
             }
         }
 
+        // -- 3rd priority: periodic actions. --
         cdllToRun = myPeriodicQ.peek();
         if(cdllToRun != 0) {
             IPeriodicAction* toRun = static_cast<IPeriodicAction*>(cdllToRun);
             if(toRun->isReadyToRun()) {
-                toRun->restartTimerNoDrift();
                 myPeriodicQ.touch();
+                ITarget::endCriticalSection();
+                toRun->restartTimerNoDrift();
                 toRun->run();
+                return;
             }
         }
 
-        // -- Run the next action from the background queue. --
+        // -- 4th priority: background actions. --
         cdllToRun = myBackgroundQ.pop();
         if(cdllToRun != 0) {
             myBackgroundQ.push(*cdllToRun);
+            ITarget::endCriticalSection();
             IBackgroundAction* toRun = static_cast<IBackgroundAction*>(cdllToRun);
             toRun->run();
             return;
         }
+        ITarget::endCriticalSection();
     }
 
 }}
